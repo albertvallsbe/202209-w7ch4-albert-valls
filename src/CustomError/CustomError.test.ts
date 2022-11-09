44e7003c271354bf2918fbf2a536731cd6197fa2
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-09 19:56:14 +0100

    Create custom error and test

---
+++ src/CustomError/CustomError.test.ts
@@ -0,0 +1,30 @@
+import e from "express";
+import CustomError from "./CustomError";
+
+describe("Given the class CustomError", () => {
+  describe("When it is instantiated with the message 'General Error', statusCode 500 and public message 'There was an error on the server'", () => {
+    test("Then it should return an object with those properties and values", () => {
+      const expectedError = {
+        message: "General Error",
+        statusCode: 500,
+        publicMessage: "There was an error on the server",
+      };
+
+      const customError = new CustomError(
+        expectedError.message,
+        expectedError.statusCode,
+        expectedError.publicMessage
+      );
+
+      expect(customError).toHaveProperty("message", expectedError.message);
+      expect(customError).toHaveProperty(
+        "statusCode",
+        expectedError.statusCode
+      );
+      expect(customError).toHaveProperty(
+        "publicMessage",
+        expectedError.publicMessage
+      );
+    });
+  });
+});
