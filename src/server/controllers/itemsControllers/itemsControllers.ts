3afeff816efb7d0e6eb80f2fb58844c2543a9b2c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-10 01:43:19 +0100

    Create get items controller

--- src/server/controllers/itemsControllers/itemsControllers.ts
+++ src/server/controllers/itemsControllers/itemsControllers.ts
@@ -1,9 +1,26 @@
-import type { NextFunction, Request, Response } from "express";
+import type { NextFunction, Response } from "express";
+import CustomError from "../../../CustomError/CustomError";
+import Item from "../../../database/models/Item";
+import type { CustomRequest } from "../usersControllers/types";
 
-export const getUserItems = (
-  req: Request,
+export const getUserItems = async (
+  req: CustomRequest,
   res: Response,
   next: NextFunction
 ) => {
-  // Const { userId } = req;
+  const { userId } = req;
+
+  try {
+    const items = await Item.find({ owner: userId });
+
+    res.status(200).json({ items });
+  } catch (error: unknown) {
+    next(
+      new CustomError(
+        (error as Error).message,
+        500,
+        "There was a problem when trying to get your list"
+      )
+    );
+  }
 };
